e2e7c72649f20fa38a0e35ee4c595adfba404f9e
Alberto Mafla Prado <79513200+A0M0305@users.noreply.github.com>
2023-08-09 14:25:39 +0100

    Migrated DB_Schemas from myPHPadmin

---
+++ database/migrations/2023_08_09_131808_create_schemas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schemas', function (Blueprint $table) {
+            $table->unsignedBigInteger('id');
+            $table->string('step')->nullable();
+            $table->string('heading')->nullable();
+            $table->string('subheading')->nullable();
+            $table->string('component')->nullable();
+            $table->string('sideheading')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schemas');
+    }
+};
